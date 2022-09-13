2da9cb5fa2bd3301e8f1fbcdd5bdade557ccd6b0
WahajRashid1020 <wahaj1020@gmail.com>
2022-09-13 16:02:32 +0500

    fifth-commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 丅ᕼᗴ ᗰᗩᖇ丅 All rights reserverd</p>
+      <p>2022 - 丅ᕼᗴ ᗰᗩᖇ丅 All rights reserverd</p>
       <p>created by Wahaj Rashid</p>
       <p className="icons">
         <AiFillInstagram />
